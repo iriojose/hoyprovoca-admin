5997904da9173c1d9a3287ab78709bf3007977f0
iriojose <iriojgomezv@gmail.com>
2020-01-17 11:15:15 -0400

    commit inicial

---
+++ src/components/AppBar.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app-bar
+        app
+        dark
+        color="red"
+    >
+
+    </v-app-bar>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+        permanent
+        height="100%"
+        width="80"
+    >
+        <v-list>
+            <v-list-item v-for="n in 5" :key="n">
+                <v-list-item-text>{{n}}</v-list-item-text>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                drawer:true
+            }
+        }
+    }
+</script>
