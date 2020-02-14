812ecf8dff4e2d4d332e49463ff9dad6956ceb12
Duccem <teamlead@somossistemas.com>
2020-02-14 16:56:43 -0400

    .

---
+++ src/components/vistasReportes/ReportesProductos.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        productos
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
--- src/views/estadisticas/Estadisticas.vue
+++ src/views/estadisticas/Estadisticas.vue
@@ -1,15 +1,15 @@
 <template>
-    <div>
-        estadisticas
-    </div>
+    <v-container>
+        <ReportesProductos />
+    </v-container>
 </template>
 
 <script>
+import ReportesProductos from '@/components/vistasReportes/ReportesProductos';
+
     export default {
-        
+        components:{
+            ReportesProductos
+        }
     }
 </script>
-
-<style lang="scss" scoped>
-
-</style>
